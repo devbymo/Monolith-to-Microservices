02cd97ed5e028ecfc1560f19525bc549ffe505a9
Mohamed475 <iammo350@gmail.com>
2023-03-10 11:33:56 +0200

    monolith version init

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=xxxxxxxxxxxx
+export POSTGRES_PASSWORD=xxxxxxxxxxxxxx
+export POSTGRES_HOST=xxxxxxxxxxxxxx
+export POSTGRES_DB=xxxxxxxxxxxx
+export AWS_BUCKET=xxxxxxxxxxxxx
+export AWS_REGION=xxxxxxxxxxxxxxx
+export AWS_PROFILE=xxxxxxxxxxxxxx
+export JWT_SECRET=xxxxxxxxxxxxxxx
+export URL=xxxxxxxxxxxxxxxx
